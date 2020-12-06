a870defa7c6f7448771523d8181f46b3bb944038
Bernardo Augusto Garcia <botibagl@gmail.com>
2020-12-06 19:05:22 +0100

    passing credentials with read

--- sample.sh
+++ sample.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-mysqldump -Fc -v -h castor-edc.mysql.database.azure.com -u bgarcial@castor-edc -p$1 -B edc-demo > test.sql
+read -sp MYSQL_PASSWORD
+mysqldump -Fc -v -h castor-edc.mysql.database.azure.com -u bgarcial@castor-edc -p"${MYSQL_PASSWORD}" -B edc-demo > test.sql
 # mysqldump -Fc -v --column-statistics=0 -h castor-edc.mysql.database.azure.com -u bgarcial@castor-edc -p$1 -B edc-demo > edc-demo_backup.sql
